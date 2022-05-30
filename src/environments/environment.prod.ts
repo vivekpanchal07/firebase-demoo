4999346a98f52165d7e3c356e9582975cd86e929
VivekPanchal <ivivekpanchal123@gmail.com>
2022-05-30 22:28:28 +0530

    firebase configured

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'fir-demo-de576',
+    appId: '1:528348798135:web:7109180df7f9be0f880132',
+    databaseURL: 'https://fir-demo-de576-default-rtdb.firebaseio.com',
+    storageBucket: 'fir-demo-de576.appspot.com',
+    apiKey: 'AIzaSyDha09E4SF2L27HZj5X15hH1Sx6A8dc_qk',
+    authDomain: 'fir-demo-de576.firebaseapp.com',
+    messagingSenderId: '528348798135',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'fir-demo-de576',
+    appId: '1:528348798135:web:7109180df7f9be0f880132',
+    databaseURL: 'https://fir-demo-de576-default-rtdb.firebaseio.com',
+    storageBucket: 'fir-demo-de576.appspot.com',
+    apiKey: 'AIzaSyDha09E4SF2L27HZj5X15hH1Sx6A8dc_qk',
+    authDomain: 'fir-demo-de576.firebaseapp.com',
+    messagingSenderId: '528348798135',
+  },
   production: false
 };
 
