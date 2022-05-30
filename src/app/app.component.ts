08f54948d9110162d0468d81eb98176324d67184
VivekPanchal <ivivekpanchal123@gmail.com>
2022-05-30 22:42:12 +0530

    aaj ka last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Database, set, ref, update, onValue,remove } from '@angular/fire/database';
+import { Data } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firebase-demoo';
+
+  constructor(public database:Database ){}
+
+  registerUser(value: any) {
+    //create data
+    // set(ref(this.database, 'users/' + value.username), {
+    //   username: value.username,
+    //   first_name: value.first_name,
+    //   profile_picture : value.last_name 
+    // }); 
+    // alert('user created!');
+
+    // read data
+    // const starCountRef = ref(this.database, 'users/' + value.username);
+    // onValue(starCountRef, (snapshot) => {
+    //   const data = snapshot.val();  
+
+    //   alert(data.first_name);   
+    // });   
+
+    // update data
+    // update(ref(this.database, 'users/' + value.username), {
+    //   // username: value.username,
+    //   first_name: value.first_name,
+    //   last_name: value.last_name
+    // });
+    // alert('user updated!');  
+    
+    //remove data
+    remove(ref(this.database, 'users/' + value.username));
+    alert('removed');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { provideDatabase,getDatabase } from '@angular/fire/database';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideDatabase(() => getDatabase())
   ],
